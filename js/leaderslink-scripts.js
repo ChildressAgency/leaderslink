7cc8d9c88d9ee7b7e4040c6f26bffbac466101f3
childressdev <jcampbell@childressagency.com>
2017-07-26 16:59:50 -0400

    home demo styled

--- js/leaderslink-scripts.js
+++ js/leaderslink-scripts.js
@@ -1,9 +1,19 @@
 jQuery(document).ready(function($){
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 50){
+      $('body').addClass('small-header');
+    }
+    else{
+      $('body').removeClass('small-header');
+    }
+  });
+
   if(typeof $.fn.lightSlider == 'function'){
-    $('#sliderBar .slider').lightSlider({
+    $('#sliderBar .slider-bar').lightSlider({
       item:3,
       loop:true,
       slideMargin:0,
+      pager:false,
       responsive:[
         {
           breakpoint:991,
@@ -18,6 +28,20 @@ jQuery(document).ready(function($){
           }
         }
       ]
+    });
+
+    $('#testimonialSlider').lightSlider({
+      item:1,
+      loop:true,
+      pager:false,
+      controls:false,
+      auto:true,
+      keyPress:true,
+      adaptiveHeight:true,
+      pause:5000,
+      onAfterSlide:function(el){
+        $(window).trigger('resize').trigger('scroll');
+      }
     });
   }
 });
