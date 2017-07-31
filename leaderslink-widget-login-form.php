8cc028280402ebcd5683fb1296f2ab5015a331bb
childressdev <jcampbell@childressagency.com>
2017-07-31 09:55:53 -0400

    projects map

--- leaderslink-widget-login-form.php
+++ leaderslink-widget-login-form.php
@@ -1,15 +1,15 @@
 <?php
 /*
-If you would like to edit this file, copy it to your current theme's directory and edit it there.
-Theme My Login will always look in your theme's directory first, before using this default template.
+  Custom login form for the sidebar
+  use theme_my_login(array('login_template' => 'leaderslink-widget-login-form.php')); to display
 */
 ?>
-<div class="tml tml-login" id="theme-my-login<?php $template->the_instance(); ?>">
+<div class="signin-widget" id="signinWidget">
 	<?php $template->the_action_template_message( 'login' ); ?>
 	<?php $template->the_errors(); ?>
 	<form name="loginform" id="loginform<?php $template->the_instance(); ?>" action="<?php $template->the_action_url( 'login', 'login_post' ); ?>" method="post">
-		<p class="tml-user-login-wrap">
-			<label for="user_login<?php $template->the_instance(); ?>"><?php
+		<div class="form-group">
+			<label for="user_login<?php $template->the_instance(); ?>" class="sr-only"><?php
 				if ( 'username' == $theme_my_login->get_option( 'login_type' ) ) {
 					_e( 'Username', 'theme-my-login' );
 				} elseif ( 'email' == $theme_my_login->get_option( 'login_type' ) ) {
@@ -18,13 +18,13 @@ Theme My Login will always look in your theme's directory first, before using th
 					_e( 'Username or E-mail', 'theme-my-login' );
 				}
 			?></label>
-			<input type="text" name="log" id="user_login<?php $template->the_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'log' ); ?>" size="20" />
-		</p>
+			<input type="text" name="log" id="user_login<?php $template->the_instance(); ?>" class="input form-control" value="<?php $template->the_posted_value( 'log' ); ?>" placeholder="Username" />
+		</div>
 
-		<p class="tml-user-pass-wrap">
-			<label for="user_pass<?php $template->the_instance(); ?>"><?php _e( 'Password', 'theme-my-login' ); ?></label>
-			<input type="password" name="pwd" id="user_pass<?php $template->the_instance(); ?>" class="input" value="" size="20" autocomplete="off" />
-		</p>
+		<div class="form-group">
+			<label for="user_pass<?php $template->the_instance(); ?>" class="sr-only"><?php _e( 'Password', 'theme-my-login' ); ?></label>
+			<input type="password" name="pwd" id="user_pass<?php $template->the_instance(); ?>" class="input form-control" value="" autocomplete="off" placeholder="password" />
+		</div>
 
 		<?php do_action( 'login_form' ); ?>
 
@@ -35,7 +35,7 @@ Theme My Login will always look in your theme's directory first, before using th
 			</p>
 
 			<p class="tml-submit-wrap">
-				<input type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php esc_attr_e( 'Log In', 'theme-my-login' ); ?>" />
+				<input type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php esc_attr_e( 'Log In', 'theme-my-login' ); ?>" class="btn-main btn-marine" />
 				<input type="hidden" name="redirect_to" value="<?php $template->the_redirect_url( 'login' ); ?>" />
 				<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
 				<input type="hidden" name="action" value="login" />
