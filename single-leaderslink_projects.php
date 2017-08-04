03f8d5fe99c1645c5d448c86c6de94b48fc732b9
childressdev <jcampbell@childressagency.com>
2017-08-04 11:55:52 -0400

    modified intro to show city_state and disaster name

--- single-leaderslink_projects.php
+++ single-leaderslink_projects.php
@@ -4,12 +4,15 @@
     <div class="container">
       <?php if(have_posts()): while(have_posts()): the_post(); ?>
         <div class="page-intro">
-          <?php if(get_field('page_title')): ?>
-            <h1><?php the_field('page_title'); ?></h1>
-          <?php endif; ?>
-          <?php if(get_field('intro_text')): ?>
-            <?php the_field('intro_text'); ?>
-          <?php endif; ?>
+          <h1><?php the_field('city_state'); ?></h1>
+          <?php 
+            if(get_field('intro_text')){
+              the_field('intro_text');
+            }
+            else{
+              the_field('disaster_name');
+            } 
+          ?>
         </div>
         <?php the_content(); ?>
       <?php endwhile; endif; ?>
