748707f24164ed164e87cd2adf5920d849813d43
John <jcampbell85.jc@gmail.com>
2017-08-07 18:00:47 -0400

    ask button widget

--- functions.php
+++ functions.php
@@ -454,3 +454,58 @@ add_action('acf/init', 'leaderslink_acf_init');
 function leaderslink_acf_init(){
 	acf_update_setting('google_api_key', get_field('google_maps_api_key', 'option'));
 }
+
+/******************************
+* ask question button widget
+******************************/
+add_action('widgets_init', 'leaderslink_load_widget');
+function leaderslink_load_widget(){
+	register_widget('leaderslink_ask_button_widget');
+}
+
+class leaderslink_ask_button_widget extends WP_Widget{
+	function __construct(){
+		parent::__construct(
+			'leaderslink_ask_button_widget',
+			__('Ask Question Button', 'leaderslink_widget_domain'),
+			array('description' => __('Show Ask a Question button', 'leaderslink_widget_domain'))
+		);
+	}
+
+	public function widget($args, $instance){
+		$title = apply_filters('widget_title', $instance['title']);
+
+		echo $args['before_widget'];
+		if(!empty($title)){
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+
+		ap_ask_btn();
+
+		echo $args['after_widget'];
+	}
+
+	public function form($instance){
+		if(isset($instance['title'])){
+			$title = $instance['title'];
+		}
+		else{
+			$title = __('New title', 'leaderslink_widget_domain');
+		}
+	?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+		</p>
+	<?php
+	}
+
+	public function update($new_instance, $old_instance){
+		$instance = array();
+		$instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+		return $instance;
+	}
+}
+/*******************************
+* end ask question button widget
+********************************/
