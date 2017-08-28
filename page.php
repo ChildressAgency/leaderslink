befab9612c5b7baedf4b1381adefb4a40f117bd4
childressdev <jcampbell@childressagency.com>
2017-08-28 10:02:00 -0400

    removed page title if empty

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
     <div class="container">
       <?php if(have_posts()): while(have_posts()): the_post(); ?>
         <div class="page-intro">
-          <h1><?php echo get_field('page_title') ? get_field('page_title') : get_the_title(); ?></h1>
+          <h1><?php echo get_field('page_title') ? get_field('page_title') : ''; ?></h1>
           <?php if(get_field('intro_text')): ?>
             <?php the_field('intro_text'); ?>
           <?php endif; ?>
