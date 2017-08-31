6f93e322697bc31fdcdcd7706f0bd8930a669a8e
childressdev <jcampbell@childressagency.com>
2017-08-31 13:31:44 -0400

    small change

--- js/leaderslink-scripts.js
+++ js/leaderslink-scripts.js
@@ -61,6 +61,18 @@ jQuery(document).ready(function($){
   $('.acf-map').each(function(){
     map = new_map($(this));
   });  
+
+  if (navigator.userAgent.match(/Trident\/7\./)) { // if IE
+    $('body').on("mousewheel", function () {
+      // remove default behavior
+      event.preventDefault();
+
+      //scroll without smoothing
+      var wheelDelta = event.wheelDelta;
+      var currentScrollPosition = window.pageYOffset;
+      window.scrollTo(0, currentScrollPosition - wheelDelta);
+    });
+  }  
 });
 
 /*
